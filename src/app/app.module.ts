a0c52cdf854522f368567be483c50827beb31f44
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-30 13:18:32 +0300

    Add the NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginviewComponent } from './loginview/loginview.component';
 import { RegisterviewComponent } from './registerview/registerview.component';
 import { HomeviewComponent } from './homeview/homeview.component';
@@ -26,7 +27,8 @@ import { PaymentviewComponent } from './paymentview/paymentview.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
