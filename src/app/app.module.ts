f68835b6a83ff998c24398aa4d9927a15737ca40
ShaviyaVictor <shaviyavictor@gmail.com>
2022-04-01 16:10:12 +0300

    Add the ng directive to the form and import the form module as well as declare the register function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,8 @@ import { DonationsapiservicesService } from './service/donationsapiservices.serv
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     DonationsapiservicesService,
